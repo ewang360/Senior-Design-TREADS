485785f4fa645400e7d7730d9decf9ff806495ad
MarkCiora <60360884+MarkCiora@users.noreply.github.com>
2023-03-22 14:33:34 -0400

    Update vid_server.py

--- vid_server.py
+++ vid_server.py
@@ -8,10 +8,10 @@ print("capture on")
 
 #HOST = "192.168.0.239"  # Standard loopback interface address (localhost)
 #HOST = "192.168.0.240"  # Standard loopback interface address (localhost)
-HOST = "10.4.93.132"
+HOST = "192.168.0.239"
 PORT = 5777  # Port to listen on (non-privileged ports are > 1023)
 
-hexstring = bytes.fromhex("F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0")
+hexstring = bytearray(bytes.fromhex("F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0"))
 step = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -20,17 +20,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("connecting...")
     conn, addr = s.accept()
     with conn:
-        print("connected")
-        success,image = vidcap.read()
-        count = 0
-        while success:
-            success,image = vidcap.read()
+        while True:
+            success,image_ = vidcap.read()
+            image = cv2.resize(image_, (100,100), interpolation = cv2.INTER_AREA)
             print(image.shape)
             success, im_buf_arr = cv2.imencode(".jpg", image)
             byte_im = im_buf_arr.tobytes()
             size = len(byte_im)
-            wait = conn.recv(64)
-            #conn.sendall(byte_im)
+            size_message = size.to_bytes(4, "little")
+            print("send: ", size_message, size)
+            #conn.send(hexstring)
+            conn.send(size_message)
             curr = 0
             size_remaining = size
             while size_remaining > 0:
@@ -39,7 +39,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 conn.send(byte_im[curr:step_target])
                 size_remaining -= step
                 curr += step
-            conn.send(hexstring)
-            time.sleep(1)
-                
-            

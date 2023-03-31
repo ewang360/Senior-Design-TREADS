cdfeb42d82268751021d0a56f2ab533a4042e0aa
MarkCiora <60360884+MarkCiora@users.noreply.github.com>
2023-03-31 13:27:05 -0400

    Updated Servos Code

--- pigpio_accel_gyro_servo.py
+++ pigpio_accel_gyro_servo.py
@@ -11,8 +11,10 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 factory = PiGPIOFactory()
 
 async def handler(websocket):
-    servo_x = Servo(21, min_pulse_width=0.4/1000,   max_pulse_width=2.325/1000, pin_factory=factory)
-    servo_y = Servo(20, min_pulse_width=0.4/1000, max_pulse_width=2.325/1000, pin_factory=factory)
+    
+    servo_x = Servo(20, min_pulse_width=0.4/1000,   max_pulse_width=2.325/1000, pin_factory=factory)
+    servo_y = Servo(21, min_pulse_width=0.4/1000, max_pulse_width=2.325/1000, pin_factory=factory)
+    servo_y.value = -0.85
     print("made connection!")
     count = 0
     while True:
@@ -21,35 +23,31 @@ async def handler(websocket):
         print(len(message))
         floats = struct.unpack('4f', message)
         print("floats: ", floats)
-        
-        #Moving head right
-        if(floats[1] <= 360.0 and floats[1] >= 270.0):
-            value_x = ((floats[1]-360) / (270-360)*-1) * (1-0) + 0
-
-        #Moving head left
-        if(floats[1] >= 0 and floats[1] <= 90):
-            value_x = ((floats[1]-0) / (90-0)) * (0-(-1)) + (-1)
-        
-        #Moving head up
-        if(floats[0] <= 360 and floats[0] >= 270):
-            value_y = ((floats[0]-360) / (270-360)*-1) * (1-0) + 0
-
-        #Moving head down
-        if(floats[0] >= 0 and floats[0] <= 90):
-            value_y = ((floats[0]-0) / (90-0)) * (0-(-1)) + (-1)
-            
-        #marks change
+                    
         value_x = floats[0]
         if value_x > 180:
             value_x -= 360
         value_x /= -90
+        if value_x > 0.5:
+            value_x = 0.5
+        if value_x < -0.5:
+            value_x = -0.5
+            
+        value_y = floats[1]
+        if value_y > 180:
+            value_y -= 360
+        value_y /= -90
+        if value_y > 0.5:
+            value_y = 0.5
+        if value_y < -0.5:
+            value_y = -0.5
             
         print("x",value_x)
         print("y",value_y)
         servo_x.value = value_x
         servo_y.value = value_y
         sleep(0.005)
-
+        
 async def main():
     async with websockets.serve(handler, "", PORT):
         await asyncio.Future()
